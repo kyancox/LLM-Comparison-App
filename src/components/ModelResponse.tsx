01b18d36633403fe101b4450a40f04fe96353df3
Kyan Cox <kyan.cox@gmail.com>
2024-05-23 18:06:07 -0400

    Commit before connecting  MongoDB

--- src/components/ModelResponse.tsx
+++ src/components/ModelResponse.tsx
@@ -6,6 +6,16 @@ interface ModelResponseProps {
 
 export default function ModelResponse({response}: ModelResponseProps) {
 
+    const formatMarkdown = (response: string) => {
+        try {
+            const parsedResponse = JSON.parse(response);
+            return parsedResponse.response;
+        } catch (error) {
+            console.error('Error parsing JSON response:', error);
+            return response;
+        }
+    }
+
     return (
         <div>
             {response && <div><ReactMarkdown>{response}</ReactMarkdown></div>}
