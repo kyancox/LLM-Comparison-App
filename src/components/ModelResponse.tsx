5fb5638c64bd344ad203b1c1dd77c422ebf7b943
Kyan Cox <kyan.cox@gmail.com>
2024-05-24 21:46:38 -0400

    Code renderer WORKING.

--- src/components/ModelResponse.tsx
+++ src/components/ModelResponse.tsx
@@ -1,24 +1,60 @@
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface ModelResponseProps {
     response: string
 }
 
-export default function ModelResponse({response}: ModelResponseProps) {
+export default function ModelResponse({ response }: ModelResponseProps) {
 
-    const formatMarkdown = (response: string) => {
-        try {
-            const parsedResponse = JSON.parse(response);
-            return parsedResponse.response;
-        } catch (error) {
-            console.error('Error parsing JSON response:', error);
-            return response;
-        }
-    }
+    // const isValidJson = (str: string) => {
+    //     try {
+    //         JSON.parse(str);
+    //         return true;
+    //     } catch (e) {
+    //         return false;
+    //     }
+    // }
+
+    // const formatMarkdown = (response: string) => {
+    //     if (isValidJson(response)) {
+    //         const parsedResponse = JSON.parse(response);
+    //         return parsedResponse.response;
+    //     } else {
+    //         console.error('Invalid JSON response');
+    //         return response;
+    //     }
+    // }
+
+    // const markdownContent = formatMarkdown(response);
 
     return (
         <div>
-            {response && <div><ReactMarkdown>{response}</ReactMarkdown></div>}
+            {response && (
+                <ReactMarkdown
+                    children={response}
+                    components={{
+                        code({ node, inline, className, children, ...props }: any) {
+                            const match = /language-(\w+)/.exec(className || '');
+                            return !inline && match ? (
+                                <SyntaxHighlighter
+                                    children={String(children).replace(/\n$/, '')}
+                                    style={oneDark}
+                                    language={match[1]}
+                                    PreTag="div"
+                                    {...props}
+                                />
+                            ) : (
+                                <code className={className} {...props}>
+                                    {children}
+                                </code>
+                            );
+                        }
+                    }}
+                />
+            )}
         </div>
     );
 }
+
