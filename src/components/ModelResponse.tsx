cb05b0f166fb9e3d6b214c643c2656eb4dae48ce
Kyan Cox <kyan.cox@gmail.com>
2024-05-30 05:53:54 -0400

    Include typewriter effect for response.

--- src/components/ModelResponse.tsx
+++ src/components/ModelResponse.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -6,59 +7,58 @@ interface ModelResponseProps {
     response: string
 }
 
+// useTypewriter hook from Stack Overflow:
+// https://stackoverflow.com/questions/77648899/react-typewriter-effect-missing-characters-issue-on-text-change
+export const useTypewriter = (text: string, speed: number) => {
+    const [displayText, setDisplayText] = useState("");
+    const idx = useRef(0);
+    const displayTextRef = useRef("");
+    useEffect(() => {
+        const typingInterval = setInterval(() => {
+            if (idx.current < text.length) {
+                displayTextRef.current += text.charAt(idx.current);
+                setDisplayText(() => displayTextRef.current);
+                idx.current += 1;
+            } else {
+                clearInterval(typingInterval);
+            }
+        }, speed);
+        return () => {
+            setDisplayText("");
+            clearInterval(typingInterval);
+        };
+    }, [text, speed]);
+    return displayText;
+};
+
 export default function ModelResponse({ response }: ModelResponseProps) {
-
-    // JSON error handling, commented out for simplification. 
-
-    // const isValidJson = (str: string) => {
-    //     try {
-    //         JSON.parse(str);
-    //         return true;
-    //     } catch (e) {
-    //         return false;
-    //     }
-    // }
-
-    // const formatMarkdown = (response: string) => {
-    //     if (isValidJson(response)) {
-    //         const parsedResponse = JSON.parse(response);
-    //         return parsedResponse.response;
-    //     } else {
-    //         console.error('Invalid JSON response');
-    //         return response;
-    //     }
-    // }
-
-    // const markdownContent = formatMarkdown(response);
+    const displayText = useTypewriter(response, 5); 
 
     return (
         <>
-            {response &&
+            {displayText &&
                 <div className='bg-logoGrey text-logoSlate rounded p-3'>
-                    {response && (
-                        <ReactMarkdown
-                            children={response}
-                            components={{
-                                // props : any type to account for TypeScript type compatibility
-                                code({ node, inline, className, children, ...props }: any) {
-                                    const match = /language-(\w+)/.exec(className || '');
-                                    return !inline && match ? (
-                                        <SyntaxHighlighter
-                                            children={String(children).replace(/\n$/, '')}
-                                            style={oneDark}
-                                            language={match[1]}
-                                            PreTag="div"
-                                            {...props}
-                                        />
-                                    ) : (
-                                        <code className={className} {...props}>
-                                            {children}
-                                        </code>
-                                    );
-                                }
-                            }}
-                        />
-                    )}
+                    <ReactMarkdown
+                        children={displayText}
+                        components={{
+                            code({ node, inline, className, children, ...props }: any) {
+                                const match = /language-(\w+)/.exec(className || '');
+                                return !inline && match ? (
+                                    <SyntaxHighlighter
+                                        children={String(children).replace(/\n$/, '')}
+                                        style={oneDark}
+                                        language={match[1]}
+                                        PreTag="div"
+                                        {...props}
+                                    />
+                                ) : (
+                                    <code className={className} {...props}>
+                                        {children}
+                                    </code>
+                                );
+                            }
+                        }}
+                    />
                 </div>
             }
         </>
